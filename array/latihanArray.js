8273ddaf6ceae63cdef218a51dac1227589e36b0
zidcraft879@gmail.com <zidcraft879@gamil.com>
2024-10-11 10:18:37 +0700

    tugas selesai

---
+++ array/latihanArray.js
@@ -0,0 +1,37 @@
+const arrAngka = [20, 40, 50, 10];
+let hasil = -1;
+
+const soal1 = () =>
+  arrAngka.map((e) => e).reduce((acc, curr) => acc + curr) / arrAngka.length;
+
+const soal2 = () => arrAngka.sort((a, b) => a - b);
+
+const soal3 = () => arrAngka.sort((a, b) => b - a)[0];
+
+const soal4 = (a) => {
+  const arr = [];
+
+  for (let i = 0; i < a; i++) {
+    if (i == 0) {
+      arr.push(0);
+    } else if (i == 1) {
+      arr.push(1);
+    } else {
+      arr.push(arr[i - 1] + arr[i - 2]);
+    }
+  }
+  return arr;
+};
+
+console.log(soal1());
+console.log(soal2());
+console.log(soal3());
+console.log(soal4(10));
+
+const arr = [20, 40, 23.4, 44];
+let ang = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  ang = arr[i];
+  console.log(ang);
+}
